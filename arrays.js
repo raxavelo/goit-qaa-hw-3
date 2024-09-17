759b539b3036de86f8a87673c28d11293d3df89d
raxavelo <raxavelo@gmail.com>
2024-09-17 11:54:47 +0200

    Solved Exercise 2

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 // Exercise 1: Creating arrays
 
 const fruits = ['apple', 'banana', 'orange', 'grape', 'watermelon'];
+
+// Exercise 2: Arrays iteration
+
+const numbers = [1, 4, 9, 13, 2, 5, 27, 2];
+
+for (const number of numbers) {
+    if (number > 5) {
+        console.log(number);
+    }
+}
